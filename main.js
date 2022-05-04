e13f220ec5d94065e627bb83a7eeeecaaa8dbc2d
turnertop <89334184+turnertop@users.noreply.github.com>
2022-05-04 14:40:36 -0700

    main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+const kelvin = 0;
+// establishing constant variable for temperature kelvin and setting it equal to 293
+
+const celsius = kelvin - 273;
+// establishing constant variable for temperature celsius and using formula from kelvin to determine it
+
+let fahrenheit = Math.floor(celsius * (9/5) + 32);
+// establishing formula to determine temperature in fahrenheit 
+
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
+
+// divider
+
+let newton = Math.floor(celsius * (33/100))
+console.log(`The temperature is ${newton} degrees Newton.`)
+
+// hot naked sex easy peasy
