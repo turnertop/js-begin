4f20cd12cbe64c5aaa0435116aa0313f7341bddd
turnertop <89334184+turnertop@users.noreply.github.com>
2022-05-04 14:50:51 -0700

    Update main.js

--- main.js
+++ main.js
@@ -15,3 +15,23 @@ let newton = Math.floor(celsius * (33/100))
 console.log(`The temperature is ${newton} degrees Newton.`)
 
 // hot naked sex easy peasy
+
+let myAge = 21
+// this is my age you doofus
+
+let earlyYears = 2
+// creating an early year variable because dogs have a different lifespan than humans, they accelerate faster in the first two years than every subsequent year
+
+earlyYears *= 10.5
+
+let laterYears = myAge -= 2
+
+laterYears *= 4
+console.log(earlyYears)
+console.log(laterYears)
+
+let myAgeInDogYears = earlyYears + laterYears
+
+const myName = "Eddie".toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
