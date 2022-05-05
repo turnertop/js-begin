68476f14bb109d635080c5ec510d66819cf5e2e7
turnertop <89334184+turnertop@users.noreply.github.com>
2022-05-04 22:48:25 -0700

    Create practice bad scoping.js

---
+++ practice bad scoping.js
@@ -0,0 +1,15 @@
+const logVisibleLightWaves = () => {
+  let lightWaves = 'Moonlight';
+	let region = 'The Arctic';
+  if (region) {
+    let lightWaves = "Northern Lights"
+  
+  
+  console.log(lightWaves);
+  }
+};
+
+logVisibleLightWaves();
+
+// holy fuck i added the fucking if statement inside for region you fucking head ass!!!!!!!!
+// this fucking question is retarded, why the fuck would i need to learn scope, ill deal with scope creep when it fucking comes and it's common sense to keep the associated variables inside the function and have global variables separated!!!!!! what the fuk!!!!
