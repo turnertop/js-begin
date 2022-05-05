8441acef4435f2c471baff212ac61893970d4509
turnertop <89334184+turnertop@users.noreply.github.com>
2022-05-04 22:08:27 -0700

    Create writing functions with arrows.js

---
+++ writing functions with arrows.js
@@ -0,0 +1,12 @@
+const plantNeedsWater = (day) => {
+  if (day === 'Wednesday') {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+// instead of writing the function all the way out,
+// you can use const (var name) = (parameter) => {
+
+// }
