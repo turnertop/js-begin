d849e50640735235c136ac5aa2cf541052d969d2
turnertop <89334184+turnertop@users.noreply.github.com>
2022-05-04 22:05:31 -0700

    Create function expressions.js

---
+++ function expressions.js
@@ -0,0 +1,13 @@
+const plantNeedsWater = function(day) {
+  if(day === "Wednesday") {
+    return true;
+  } else {
+    return false;
+};
+}
+plantNeedsWater("Tuesday");
+console.log(plantNeedsWater("Tuesday"))
+
+// holy fuck this one was so fucking aids
+// i forgot the } at the end of the function to close this shit
+// also i didn't know when you print the damn thing, you need to explicitly state the entire function with the parameter of Tuesday inside of it as well, cringe as fuck javascript's entire family i will get ri o
