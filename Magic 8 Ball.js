2640af2ec7ff699861b921e8a0e6f18aad793808
turnertop <89334184+turnertop@users.noreply.github.com>
2022-05-04 18:56:26 -0700

    Create Magic 8 Ball.js

---
+++ Magic 8 Ball.js
@@ -0,0 +1,47 @@
+let userName = "";
+
+if (userName === "Jane") {
+  console.log ("Hello Jane!");
+} else {
+  console.log ("Hello!");
+}
+
+let userQuestion = "How to get women?";
+switch (userQuestion) {
+  case "How to get women?":
+  console.log("I don't know because I don't get any women myself.")
+  break
+}
+
+let randomNumber = Math.floor(Math.random() * 8);
+switch (randomNumber) {
+  case 0:
+  console.log("Fuck this shit!")
+  break
+  case 1:
+  console.log("This shit is so stupid!")
+  break
+  case 2:
+  console.log("You're entire family!")
+  break
+  case 3:
+  console.log("I will get rid of them!")
+  break
+  case 4:
+  console.log("This library is the worst!")
+  break
+  case 5:
+  console.log("The end of the world is near.")
+  break
+  case 6:
+  console.log("I will bring about the end of everything.")
+  break
+  case 7:
+  console.log("I have become death. The destroyer of worlds.")
+  break
+  case 8:
+  console.log("The very framework of this world can be adjusted.")
+}
+let eightBall = ""
+// i think I fucked it up because you were supposed to add the strings to the eightball and have randomnumber correspond with the eightball, but i didn't do that.
+// oh well too lazy to fix this dumbass shit, time to progress in knowledge to become the greatest there ever was.
